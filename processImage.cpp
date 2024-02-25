26904e14dc373e8d63d76bd34b2518f4f46d7471
susukacang <xircambyses@yahoo.com>
2024-02-24 21:09:02 -0800

    Update processImage.cpp

--- processImage.cpp
+++ processImage.cpp
@@ -1,57 +1,69 @@
 #include <iostream>
 // #include <vector>
 // #include <string>
-#include <unistd.h>
+//#include <unistd.h>
 #include "CImg.h"
 using namespace std;
 using namespace cimg_library;
 
+#include <windows.h>
 
-CImg<unsigned char> normalize(const CImg<unsigned char> &img) {
-  int width = img.width();
-  int height = img.height();
-  CImg<unsigned char> tmp(width, height, 1, 1, 0);
-  float min = 10000; float max = 0;
-  
-  for (int r = 0; r < height; ++r) {
-    for (int c = 0; c < width; ++c) {
-      if( img(r, c) < min) min = img(r, c);
-      if( img(r, c) > max) max = img(r, c);
-    }
-  }
-
-  for (int r = 0; r < height; ++r) {
-    for (int c = 0; c < width; ++c) {
-      float ratio = (float) (img(r, c) - min) / (max - min);
-      tmp(r, c) = ratio * 255;
-      if (r >= 60 && r <= 63 && c >= 60 && c <= 63)
-        std::cout << ratio * 255 << " ";
-    }
-    // std::cout << "\n";
-  }
-
-  return tmp;
+void usleep(__int64 usec) {
+    HANDLE timer;
+    LARGE_INTEGER ft;
+    ft.QuadPart = -(10 * usec); // Convert to 100 nanosecond interval (negative value indicates relative time)
+    timer = CreateWaitableTimer(NULL, TRUE, NULL);
+    SetWaitableTimer(timer, &ft, 0, NULL, NULL, 0);
+    WaitForSingleObject(timer, INFINITE);
+    CloseHandle(timer);
 }
 
-CImg<unsigned char> calcGradientMagnitude(const CImg<unsigned char> &sobel_x,
-                                          const CImg<unsigned char> &sobel_y) {
-  int width = sobel_x.width();
-  int height = sobel_x.height();
-  CImg<unsigned char> tmp(width, height, 1, 1, 0);
 
-  for (int r = 0; r < height; ++r) {
-    for (int c = 0; c < width; ++c) {
-      tmp(r, c) =
-          sqrt(sobel_x(r, c) * sobel_x(r, c) + sobel_y(r, c) * sobel_x(r, c));
-      // if (tmp(r, c) > 255)
-      //   tmp(r, c) = 255;
+CImg<unsigned char> normalize(const CImg<unsigned char>& img) {
+    int width = img.width();
+    int height = img.height();
+    CImg<unsigned char> tmp(width, height, 1, 1, 0);
+    float min = 10000; float max = 0;
+
+    for (int r = 0; r < height; ++r) {
+        for (int c = 0; c < width; ++c) {
+            if (img(r, c) < min) min = img(r, c);
+            if (img(r, c) > max) max = img(r, c);
+        }
     }
-  }
 
-  return tmp;
+    for (int r = 0; r < height; ++r) {
+        for (int c = 0; c < width; ++c) {
+            float ratio = (float)(img(r, c) - min) / (max - min);
+            tmp(r, c) = ratio * 255;
+            if (r >= 250 && r <= 255 && c >= 250 && c <= 255)
+                std::cout << ratio * 255 << " ";
+        }
+        // std::cout << "\n";
+    }
+
+    return tmp;
 }
 
-CImg<unsigned char> processImage(const CImg<unsigned char> &img, int type = 0)
+CImg<unsigned char> calcGradientMagnitude(const CImg<unsigned char>& sobel_x,
+    const CImg<unsigned char>& sobel_y) {
+    int width = sobel_x.width();
+    int height = sobel_x.height();
+    CImg<unsigned char> tmp(width, height, 1, 1, 0);
+
+    for (int r = 0; r < height; ++r) {
+        for (int c = 0; c < width; ++c) {
+            tmp(r, c) =
+                sqrt(sobel_x(r, c) * sobel_x(r, c) + sobel_y(r, c) * sobel_x(r, c));
+            // if (tmp(r, c) > 255)
+            //   tmp(r, c) = 255;
+        }
+    }
+
+    return tmp;
+}
+
+CImg<unsigned char> processImage(const CImg<unsigned char>& img, int type = 0)
 {
     int width = img.width();
     int height = img.height();
@@ -63,7 +75,7 @@ CImg<unsigned char> processImage(const CImg<unsigned char> &img, int type = 0)
     // identity // CMU homework kernel
     // int mask[3][3] = {{0,0,0},{0,1,0},{0,0,0}}; //{{1,3,1},{3,5,3},{1,3,1}};
     // int mask[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}}; //{{1,3,1},{3,5,3},{1,3,1}};//horizontal Sobel kernel
-    int mask[3][3] = {{-1,-2,1},{0,0,0},{1,2,1}}; //{{1,3,1},{3,5,3},{1,3,1}};//vertical Sobel kernel
+    int mask[3][3] = { {-1,-2,1},{0,0,0},{1,2,1} }; //{{1,3,1},{3,5,3},{1,3,1}};//vertical Sobel kernel
 
     if (type == 1) {    //Gy
         mask[0][0] = -1;
@@ -105,7 +117,7 @@ CImg<unsigned char> processImage(const CImg<unsigned char> &img, int type = 0)
     // numrows = sizeof(mask)/sizeof(mask[0]);
     // numcols = sizeof(mask[0])/sizeof(mask[0][0]);
 
-    int m = sizeof(mask)/sizeof(mask[0]) * 0.5;
+    int m = sizeof(mask) / sizeof(mask[0]) * 0.5;
     // cout << "m=" << m << endl;
     // int size = sizeof(mask)/sizeof(int);
     // cout << width << " " << height << endl;
@@ -113,11 +125,11 @@ CImg<unsigned char> processImage(const CImg<unsigned char> &img, int type = 0)
     {
         for (int c = 0; c < width; ++c)
         {
-            for (int i = 0; i < sizeof(mask)/sizeof(mask[0]); i++) {
-                for (int j = 0; j < sizeof(mask)/sizeof(mask[0]); j++) {
-                    if( r+i-m > 0 && r+i-m < height && c+j-m > 0 && c+j-m < width)
-                        tmp(r,c) += img(r+i-m,c+j-m) * mask[i][j]; // 1+1 for 5x5; 1 for 
-                    if((int)tmp(r,c,0,0) > 128) std:cout<< (int)tmp(r,c,0,0) << " ";
+            for (int i = 0; i < sizeof(mask) / sizeof(mask[0]); i++) {
+                for (int j = 0; j < sizeof(mask) / sizeof(mask[0]); j++) {
+                    if (r + i - m > 0 && r + i - m < height && c + j - m > 0 && c + j - m < width)
+                        tmp(r, c) += img(r + i - m, c + j - m) * mask[i][j]; // 1+1 for 5x5; 1 for 
+                    //if ((int)tmp(r, c, 0, 0) > 128) std:cout << (int)tmp(r, c, 0, 0) << " ";
                 }
             }
             // tmp(r+m,c+m) /= 256;
@@ -133,24 +145,24 @@ int processImageMain()
 {
 
     // CImg<unsigned char> img("/home/jaccobs/bin/projects/learnCPP/cimg_project/lena_128.png");
-    CImg<unsigned char> img("/home/jaccobs/bin/projects/learnCPP/cimg_project/lena_512.png");
+    CImg<unsigned char> img("lena_512.png");
 
     // img.display("My first CImg code");
-    CImgDisplay disp_orig(img.width(), img.height());
-    disp_orig.render(img);
+    CImgDisplay disp_orig(img.width(), img.height(),"Original");
+    disp_orig.display(img); // use display, not render (see tutorial.cpp inside CImg examples folder)
 
 
-    CImg<unsigned char> grayscale_img(img,img.width(), img.height(),1,1);
-    CImgDisplay dispGray(img.width(), img.height());
-    // dispGray.render(grayscale_img);
+    CImg<unsigned char> grayscale_img(img, img.width(), img.height(), 1, 1);
+    CImgDisplay dispGray(img.width(), img.height(),"Gray");
+    dispGray.display(grayscale_img);
 
 
     CImg<unsigned char> sobel_x = processImage(grayscale_img, 0);
     CImg<unsigned char> sobel_y = processImage(grayscale_img, 1);
     CImg<unsigned char> gradient_magnitude = calcGradientMagnitude(sobel_x, sobel_y);
-    // gradient_magnitude = normalize(gradient_magnitude);
-
-    dispGray.render(gradient_magnitude);
+    gradient_magnitude = normalize(gradient_magnitude);
+    CImgDisplay dispSobel(img.width(), img.height(),"Sobel");
+    dispSobel.display(gradient_magnitude);
 
     // CImg<unsigned char> processedImage2(128,128,1,1);// = processImage(grayscale_img, 0);
     // processedImage2.fill(0);
@@ -159,10 +171,15 @@ int processImageMain()
     // // grayscale_img = processImage(grayscale_img, 1);
     // dispGray.render(grayscale_img);
 
-    while (!disp_orig.is_closed() && !dispGray.is_closed())
+    while (!disp_orig.is_closed() && !dispGray.is_closed() && !dispSobel.is_closed())
     {
-        usleep(100000);
+        //usleep(100000);
         // disp.display(img2);
+        if (disp_orig.is_resized())disp_orig.resize().display(img);
+        if (dispGray.is_resized()) dispGray.resize().display(grayscale_img);
+        if (dispSobel.is_resized()) dispSobel.resize().display(gradient_magnitude);
+        // Temporize event loop
+        cimg::wait(20);
     }
     // disp.display(img);
 
